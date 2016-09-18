25bd82793dd5cf3ebf7deebc95d281575f44aa17
xiangyuhui <xiangyuhui@wangdongdeiMac.local>
2016-09-18 11:21:03 +0800

    更新远程和本地服务器的版本

---
+++ ShenmaoProject/Classes/Common/Category/UIView+showHUD.h
@@ -0,0 +1,13 @@
+//
+//  UIView+showHUD.h
+//  ShenmaoProject
+//
+//  Created by xiangyuhui on 16/9/18.
+//  Copyright © 2016年 神猫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (showHUD)
+
+@end
